ada53e46407f70e4ea56a4460f7b5fc6d230699b
Chaz <cmtlum73@gmail.com>
2015-12-04 16:48:03 -1000

    Finished and added notes to answers

--- context.js
+++ context.js
@@ -1,7 +1,7 @@
 var Robot = function (name) {
   this.name = name;
 
-}
+};
 
 function add ( op1, op2 ) {
   this.name = this.name || "Humans";
@@ -16,15 +16,24 @@ var bmo = new Robot("B-Mo");
 var wall_e = new Robot("Wall-E");
 
 
-// #1
-console.log(  );
 
-// #2  
-console.log(  );
+
+// #1
+console.log(add(0, 1));
+// invoke the add function to the global context, this.name is undefined in the global context so replace with "Humans" and pass arguments 0 and 1 to add function
+
+
+// #2
+console.log(add.call(voltron, 2, 3));
+//invoke the add function, set context to voltron with call and pass arguments 2 and 3 to add function
+
 
 // #3
-console.log(  );
+console.log(add.apply(optimus, [20, 30]));
+//invoke the add function, set context to optimus with apply and pass the array containing 20 and 30 to add function
+
 
 // #4
-
-console.log(  );
+var calculate = add.bind(bender);
+console.log(calculate('drinking', ' beer'));
+//bind the add function to the variable calculate and set the context to bender *calculate does not run the add function yet!*, invoke calculate and pass arguments 'drinking' and 'beer' to add function
